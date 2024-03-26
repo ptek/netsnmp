5e29ca8f08aacf3bbf62b77e59ff22488ca3f7e6
Pavlo Kerestey <pavlo@kerestey.net>
2024-03-26 11:12:30 +0100

    Fix the tests, and bump the major version

--- tests/SystemTest/Platform.hs
+++ tests/SystemTest/Platform.hs
@@ -2,23 +2,24 @@ module SystemTest.Platform (tests) where
 
 import Control.Exception.Base
 import Network.Protocol.NetSNMP
-import Test.HUnit
 import System.Cmd
+import Test.HUnit
 
-tests = test [
-   "snmpGet returns correct OID" ~: do
-     givenSnmpdRunning
-     o <- (snmpGet snmp_version_2c "127.0.0.1:6161" "HSTEST" [1,3,6,1,2,1,1,4,0] >>= either error (return . oid)) `finally` killSnmpd
-     o @?= [1,3,6,1,2,1,1,4,0]
- ]
+tests =
+  test
+    [ "snmpGet returns correct value" ~: do
+        givenSnmpdRunning
+        o <- (snmpGet snmp_version_2c "127.0.0.1:6161" "HSTEST" [1, 3, 6, 1, 2, 1, 2, 2, 1, 2, 1] >>= either error (return . value)) `finally` killSnmpd
+        o @?= OctetString "GigabitEthernet" [71, 105, 103, 97, 98, 105, 116, 69, 116, 104, 101, 114, 110, 101, 116]
+    ]
 
 givenSnmpdRunning :: IO ()
 givenSnmpdRunning = do
   system $ "cp -f tests/mock.snmpd.conf " ++ mockConfigFilePath
-  system ("PATH=$PATH:/usr/sbin:/usr/local/sbin snmpd -r -C -c "++mockConfigFilePath++" 127.0.0.1:6161") >> return ()
+  system ("PATH=$PATH:/usr/sbin:/usr/local/sbin snmpd -r -C -c " ++ mockConfigFilePath ++ " 127.0.0.1:6161") >> return ()
 
 killSnmpd :: IO ()
-killSnmpd = rawSystem "pkill" ["-9","-f","snmpd"] >> return ()
+killSnmpd = rawSystem "pkill" ["-9", "-f", "snmpd"] >> return ()
 
 mockConfigFilePath :: FilePath
 mockConfigFilePath = "/tmp/Network.Protocol.NetSNMPtest.snmpd.conf"
